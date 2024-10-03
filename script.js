a9cae47a2c576acfd61c417af373194e0084925b
Mathew090 <oyebodemathew555@gmail.com>
2024-10-03 01:15:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = .document.querySelector (".menu-links");
+    const icon = .document.querySelector (".hamburger-icon");
+    menu.classlist.toggle("open")
+    icon.classlist.toggle("open")
+}
